4dd4bc2d56ebd81ec2f5c3261598c0f47cb3ac3a
devgamez2022 <101201095+devgamez2022@users.noreply.github.com>
2023-09-10 07:41:44 -0600

    Metodo para consultar datos

---
+++ lib/models/crud.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getStudiantes() async {
+  List students = [];
+  CollectionReference collectionReference = db.collection('tb_students');
+  QuerySnapshot querySnapshot = await collectionReference.get();
+  /*
+  for (var documento in querySnapshot.docs) {
+    students.add(documento.data());
+  }
+  */
+  querySnapshot.docs.forEach((documento) {
+    students.add(documento.data());
+  });
+  return students;
+}
